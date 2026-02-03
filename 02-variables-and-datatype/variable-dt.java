847585118bcb5a75beedf0f264458311624c393a
Vidyashree <137021847+Vidyashree-79@users.noreply.github.com>
2026-02-03 12:50:10 +0530

    Create Variables class to demonstrate data types
    Added a class to demonstrate variable declaration and initialization in Java.

---
+++ 02-variables-and-datatype/variable-dt.java
@@ -0,0 +1,18 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        // Variable Declaration and Initialization
+        int a = 10;
+        double price = 99.5;
+        String language = "Java";
+
+        System.out.println(a);
+        System.out.println(price);
+        System.out.println(language);
+
+        // Local Variable Example
+        int number = 5;
+        System.out.println(number);
+    }
+}
